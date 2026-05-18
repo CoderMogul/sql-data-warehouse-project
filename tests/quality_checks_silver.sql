a781585aca64ce94a6bffa2fd0ae36c31bd3a999
CodeMogul <107785023+CoderMogul@users.noreply.github.com>
2026-05-18 22:51:32 +0100

    Implement quality checks for silver schema data
    Added comprehensive quality checks for data consistency and accuracy across the silver schema, including checks for nulls, duplicates, unwanted spaces, data standardization, and invalid date ranges.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,406 @@
+/*
+===================================================================================
+Quality Checks
+===================================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy,
+    and standardization across the 'silver' schema. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after loading the Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks
+====================================================================================
+*/
+
+
+-- ============================================
+-- Check & Clean Bronze layer for Silver layer
+-- ============================================
+-- Check 1:
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) >1 OR cst_id IS NULL
+
+-- Check 2:
+-- Check for unwanted Spaces
+-- Expectation: No Results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+-- Check 3: 
+-- Check for Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM bronze.crm_cust_info
+
+-- =================================================
+-- Data Quality Check for Silver layer After Insert
+-- =================================================
+SELECT
+cst_id,
+COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) >1 OR cst_id IS NULL
+
+-- Check 2:
+-- Check for unwanted Spaces
+-- Expectation: No Results
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+SELECT cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+-- Check 3: 
+-- Check for Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+-- Final check
+SELECT * FROM silver.crm_cust_info
+
+
+-- =============================================
+-- Cleaning the Bronze Layer - crm_prd_info
+-- =============================================
+
+SELECT
+	prd_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+FROM bronze.crm_prd_info
+
+-- Check 1:
+-- Check For Null or Duplicate in Primary Key
+-- Expectation: No Duplicate
+SELECT
+prd_id,
+COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+
+-- Check for unwanted 
+-- Expectation: No Results
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm) OR prd_nm IS NULL
+
+-- Check for NULL or negative numbers
+-- Expectation: No Result
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--Check for Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+-- Check for Invalid Date Orders
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT
+	prd_id,
+	prd_key,
+	prd_nm,
+	prd_start_dt,
+	prd_end_dt,
+	LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_test
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
+
+
+-- =============================================== 
+-- Quality check after Insert silver.crm_prd_info
+-- ===============================================
+
+-- Check 1:
+-- Check For Null or Duplicate in Primary Key
+-- Expectation: No Duplicate
+SELECT
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+
+-- Check for unwanted 
+-- Expectation: No Results
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm) OR prd_nm IS NULL
+
+-- Check for NULL or negative numbers
+-- Expectation: No Result
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--Check for Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for Invalid Date Orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT
+	prd_id,
+	prd_key,
+	prd_nm,
+	prd_start_dt,
+	prd_end_dt,
+	LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_test
+FROM silver.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
+
+-- =================================================
+-- DATA CLEANING & PREPARATION FOR THE SILVER LAYER
+-- =================================================
+-- CLEANING SILVER SALES DETAILS TABLE (crm_sales_details)
+-- ----------------------------------------------------------
+
+-- Checking for Unwanted Spaces
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+-- Check for Invalid Dates
+-- Change all 0 if any to NULL
+SELECT
+NULLIF(sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR
+	  sls_order_dt > 20500101 OR
+	  sls_order_dt < 19000101 OR
+	  LEN(sls_order_dt) != 8
+
+
+SELECT
+NULLIF(sls_ship_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101 
+OR LEN(sls_ship_dt) != 8
+
+SELECT
+sls_order_dt
+FROM bronze.crm_sales_details
+WHERe sls_order_dt != 8
+
+-- Check for Invalid Date Orders
+-- Expectation: Order date should not be higher than due date or ship date
+SELECT
+*
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt
+OR sls_order_dt > sls_due_dt
+
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, Zero, or negative.
+
+SELECT DISTINCT
+sls_sales AS old_sls_sales,
+sls_quantity,
+sls_price AS old_sls_price,
+
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+		THEN sls_sales / NULLIF(SLS_quantity, 0)
+	ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL 
+OR sls_quantity IS NULL
+OR sls_price IS NULL
+OR sls_sales <= 0 
+OR sls_quantity <0
+OR sls_price <= 0 
+ORDER BY sls_sales, sls_quantity,sls_price
+
+
+-- ======================================================
+-- After Inserting into the silver_crm_sales table.
+-- Do Quality Check on the silver_crm_sales table
+-- ======================================================
+
+-- Check for Invalid Date Orders
+-- Expectation: Order date should not be higher than due date or ship date
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt
+OR sls_order_dt > sls_due_dt
+
+
+-- Check for Data consistency in Price, Quantity & Sales
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL 
+OR sls_quantity IS NULL
+OR sls_price IS NULL
+OR sls_sales <= 0 
+OR sls_quantity <0
+OR sls_price <= 0 
+ORDER BY sls_sales, sls_quantity,sls_price
+
+SELECT
+cid,
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	 ELSE cid
+END AS cid,
+bdate,
+gen
+FROM bronze.erp_cust_az12
+WHERE CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	  ELSE cid
+      END
+	NOT IN (SELECT DISTINCT cst_key FROM silver.crm_cust_info)
+
+
+
+-- ----------------------------------------------------------
+-- Identify Out of Ranage Dates
+SELECT DISTINCT
+bdate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Check Data Standardization & Consistency
+SELECT DISTINCT gen
+FROM bronze.erp_cust_az12
+
+
+-- ==========================================================
+-- Data Quality Check after INSERT into silver.erp_cust_az12
+-- ===========================================================
+SELECT DISTINCT
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Check Data Standardization & Consistency
+SELECT DISTINCT gen
+FROM silver.erp_cust_az12
+
+
+SELECT
+REPLACE(cid, '-', '') AS cid,
+cntry
+FROM bronze.erp_loc_a101
+WHERE REPLACE(cid, '-', '') NOT IN
+(SELECT cst_key FROM silver.crm_cust_info)
+
+SELECT cst_key FROM silver.crm_cust_info
+
+-- Check for Clean data
+-- Check Country column
+SELECT DISTINCT cntry
+FROM bronze.erp_loc_a101
+ORDER BY cntry
+
+SELECT DISTINCT
+cntry AS old_cntry,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'GERNMANY'
+	 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101 
+
+-- ============================
+-- Quality check after insert
+-- ============================
+SELECT DISTINCT cntry
+FROM silver.erp_loc_a101
+ORDER BY cntry
+
+
+-- ============================ 
+-- Cleaning of erp_px_cat_g1v2
+-- ============================
+
+SELECT
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2
+
+-- Check for Unwanted spaces
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+
+
+-- Data Standardization & COnsistency
+SELECT DISTINCT
+cat
+FROM bronze.erp_px_cat_g1v2
+-- The table is clean
+
+
+
+
 
